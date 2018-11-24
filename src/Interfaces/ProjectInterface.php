e27580e8d5be95702aed3ea1dd83210659f6e123
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-11-24 18:50:50 +0700

    Update package

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -17,8 +17,8 @@ namespace nguyenanhung\MantisBT\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION             = '1.0.1';
-    const LAST_MODIFIED       = '2018-11-11';
+    const VERSION             = '1.0.2';
+    const LAST_MODIFIED       = '2018-11-24';
     const AUTHOR_NAME         = 'Hung Nguyen';
     const AUTHOR_EMAIL        = 'dev@nguyenanhung.com';
     const PROJECT_NAME        = 'Mantis Bug Tracker Connector';
